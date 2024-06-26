c16fa45d80ae506dc2cbdeb0ad8d38aa8932cdb7
Artur <artcitycreativ@gmail.com>
2024-06-26 12:19:38 +0300

    Зайнуллин Артур. Домашняя работа. Исправления.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,53 +1,26 @@
-import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
-    Scanner scanner = new Scanner(System.in);
-    ArrayList<Car> carList = new ArrayList<>();
-    Race race = new Race();
-    String commad = "start";
+    static Race race = new Race();
 
-    public void main(String[] args) {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String command = "start";
+
         while(true)
         {
-            if(Objects.equals(commad, "start"))
+            if(Objects.equals(command, "start"))
             {
-                this.commandStart();
-            } else if(Objects.equals(commad, "exit"))
+                if(race.commandStart())
+                {
+                    command = scanner.nextLine();
+                }
+            } else if(Objects.equals(command, "exit"))
             {
+                System.out.println("Завергаем работу.");
                 break;
             }
-        }
-    }
-    public void commandStart()
-    {
-        if(carList.isEmpty())
-        {
-            System.out.println("Введите данные 3-х автомобилей для начала гонки.\nВ этой гонке только одно правило: Допускаются автомобили со скоростью не выше 250 км/ч");
-        }
-        if(carList.size() < 3) {
-            addCars();
-        } else {
-            race.start(carList);
-            carList.clear();
-            commad = scanner.nextLine();
-        }
-    }
-    public void addCars()
-    {
-        System.out.println("Введите название " + (carList.size() + 1) + "-го автомобиля");
-        String name = scanner.nextLine();
-
-        System.out.println("Введите скорость автомобиля '"+ name +"'");
-        int speed = scanner.nextInt();
-        if (speed >= 0 && speed <= 250) {
-            carList.add(new Car(name, speed));
-            System.out.println("Введите скорость автомобиля (0-250)");
-        } else if(speed < 0) {
-            System.out.println("С этим автомобилем что-то не так. Данный автомобиль дисквалифицирован.");
-        } else {
-            System.out.println("Превышена максимальная скорость автомобиля. Данный автомобиль дисквалифицирован.");
         }
     }
 }
--- src/main/java/Race.java
+++ src/main/java/Race.java
@@ -1,8 +1,13 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Race {
+    ArrayList<Car> carList = new ArrayList<>();
+
+    Scanner scanner = new Scanner(System.in);
     String leader;
     int distance = 0;
+    String command = "start";
 
     private void newLeader(String carName,int speed) {
         int distance = 24 * speed;
@@ -15,17 +20,53 @@ public class Race {
     {
         for(int i = 1; i <= 3; i++) {
             try {
-                Thread.sleep(2000);
+                Thread.sleep(1000);
+                System.out.println(i + "й круг пройден!");
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-            System.out.println(i + "й круг пройден!");
+        }
+    }
+    public boolean commandStart()
+    {
+        if(carList.isEmpty())
+        {
+            System.out.println("Введите данные 3-х автомобилей для начала гонки.\nВ этой гонке только одно правило: Допускаются автомобили со скоростью не выше 250 км/ч");
+        }
+        if(carList.size() < 3) {
+            addCars();
+        } else {
+            this.start(carList);
+            carList.clear();
+            return true;
+        }
+        return false;
+    }
+    public void addCars()
+    {
+        System.out.println("Введите название " + (carList.size() + 1) + "-го автомобиля");
+        String name = scanner.next();
+
+        System.out.println("Введите скорость автомобиля '"+ name +"' (0-250)");
+        int speed = scanner.nextInt();
+        if (speed >= 0 && speed <= 250) {
+            carList.add(new Car(name, speed));
+
+        } else if(speed < 0) {
+            System.out.println("С этим автомобилем что-то не так. Данный автомобиль дисквалифицирован.");
+        } else {
+            System.out.println("Превышена максимальная скорость автомобиля. Данный автомобиль дисквалифицирован.");
         }
     }
     public void start(ArrayList<Car> carList)
     {
         for(int i = 3; i > 0; i--) {
-            System.out.println(i);
+            try {
+                System.out.println(i);
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
         System.out.println("СТАРТ!");
         this.rounds();
