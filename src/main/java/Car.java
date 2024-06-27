4217b8374be67e4c3e99411b2672dee28d9bc6dd
Artur <artcitycreativ@gmail.com>
2024-06-27 10:44:29 +0300

    Зайнуллин Артур. Домашняя работа. Исправления.

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -6,10 +6,4 @@ public class Car {
         this.name = name;
         this.speed = speed;
     }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public void setSpeed(int speed) {
-        this.speed = speed;
-    }
 }
