bd12c1fdea4b89ea4d459d69471e82dcf7cc99a3
Artur <artcitycreativ@gmail.com>
2024-06-27 10:44:29 +0300

    Зайнуллин Артур. Домашняя работа. Исправления.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
+
     static Race race = new Race();
 
     public static void main(String[] args) {
@@ -18,8 +19,11 @@ public class Main {
                 }
             } else if(Objects.equals(command, "exit"))
             {
-                System.out.println("Завергаем работу.");
+                System.out.println("Завершаем работу.");
                 break;
+            } else {
+                System.out.println("Комманда не распознана.");
+                command = scanner.nextLine();
             }
         }
     }
